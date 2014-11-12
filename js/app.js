7b63cca05dab52e0b63df6afcc41ad55bf7c65b0
tanasiliev <tanasiliev@gmail.com>
2014-11-13 00:14:09 +0200

    change background image

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ define([
     var initialize = function () {
 
         document.body.style.height = window.innerHeight - 20 + 'px';
+        document.body.style.backgroundSize = window.innerWidth + "px " + window.innerHeight + "px";
         var icon = new IconView("text.txt", "doc");
         var icon1 = new IconView("img.png", "img");
         var icon2 = new IconView("MyPdf.pdf", "pdf");
